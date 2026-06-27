937194607a52c18916b856ef13ab3b1dc6f39903
Roberts <123892258+universo_emprende@users.noreply.github.com>
2026-06-27 10:28:31 -0400

    SWUE-2 index.php final

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <h1>UDEMY1</h1>
 </head>
 <body>
 
-    <! -- <h1>UDEMY</h1> -->
+     <h1>UDEMY</h1>
     
 </body>
 </html>
